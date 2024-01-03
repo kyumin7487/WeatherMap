1f5625822dda5f80ef56be7089c4b5af6f736183
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-01-03 21:27:56 +0900

    :sparkles: Feature :: API연결해서 도시의 날씨와 온도 띄우기
    API키를 개인적으로 발급받아 사용해주세요
    API키를 처음 발급받으면 API를 받아오는데 시간이 걸립니다 좀 기다려 주세요

--- weathermap/src/App.js
+++ weathermap/src/App.js
@@ -1,11 +1,52 @@
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
+  const API_KEY = "6bf6b9fcc70b186f91f2aff88b83dbab";   // 각자 개인의 API KEY를 발급받아 사용해주세요. 내꺼 쓰면 혼나용
+  const [locatiion, setLocation] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=$(location)&appid=$(API_KEY)`;
+  const [result, setResult] = useState({});
 
+  const searchWeather = async (e) => {
+    if(e.key === 'Enter') {
+      try {
+        const data = await axios({
+          method: 'get',
+          url: url,
+        })
+        console.log(data);
+        setResult(data);
+      } 
+      catch(err) {
+        alert(err);
+      }
+    }
+  }
+
+  return (
+    <div className='Main'>
+      <div className='appContentWrap'>
+        <input 
+          placeholder='도시를 입력하세요'
+          value={locatiion}
+          onChange={(e)=>setLocation(e.target.value)}
+          type='text'
+          onKeyDown={searchWeather}
+        />
+        {
+          Object.keys(result).length !== 0 && ( // 우리가 보는 온도 작업때문에 temperature가 더 코드가 많음
+            <div className='ResultWrap'>
+            <div className='city'>{result.data.name}</div>
+            <div className='temperature'> 
+              {Math.round(result.data.main.temp -273.15 * 10) /10}°C
+            </div>
+            <div className='sky'>{result.data.weather[0].main}</div>
+          </div>
+          )
+        }
+      </div>
     </div>
   );
 }
-
 export default App;
